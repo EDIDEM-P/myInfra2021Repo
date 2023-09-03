1bfbe6a6a0b52b13615daa5d22cd85631e380984
EDIDEM-P <139933090+EDIDEM-P@users.noreply.github.com>
2023-09-03 03:58:44 -0500

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mySonarInstance" {
       ami           = "ami-0ee23bfc74a881de5"
-      key_name = var.key_name
-      instance_type = "t2.micro"
+      key_name = var.OBONG1
+instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
       tags= {
         Name = "sonar_instance"
